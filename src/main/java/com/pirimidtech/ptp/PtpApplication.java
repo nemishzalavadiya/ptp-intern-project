fa6e26f7707031982ea4b507c6256e66523b63db
Chintan Sheth <chintan@pirimidtech.com>
2021-01-25 10:32:11 +0530

    Initial Scaffolding

---
+++ src/main/java/com/pirimidtech/ptp/PtpApplication.java
@@ -0,0 +1,13 @@
+package com.pirimidtech.ptp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pirimidtech/ptp/PtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pirimidtech.ptp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

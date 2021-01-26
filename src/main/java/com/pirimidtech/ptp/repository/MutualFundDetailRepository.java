9d52a782f3cf5d3fcabbdb5db0a084c4a5c321ee
devesh.ahuja <devesh.ahuja@pirimidtech.com>
2021-01-26 14:41:25 +0530

    repositories added

---
+++ src/main/java/com/pirimidtech/ptp/repository/MutualFundDetailRepository.java
@@ -0,0 +1,10 @@
+package com.pirimidtech.ptp.repository;
+
+import com.pirimidtech.ptp.entity.MutualFundDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MutualFundDetailRepository extends JpaRepository<MutualFundDetail, UUID> {
+
+}

46b7ddc86e7c3044dd4f9ec4264e3721e9e7961b
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-01-29 12:59:24 +0530

    watchlist show api

---
+++ src/main/java/com/pirimidtech/ptp/service/watchlist/WatchlistServiceInterface.java
@@ -0,0 +1,10 @@
+package com.pirimidtech.ptp.service.watchlist;
+
+import com.pirimidtech.ptp.entity.Watchlist;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface WatchlistServiceInterface {
+    List<Watchlist> getAllStockDetailByUserId(UUID userId);
+}

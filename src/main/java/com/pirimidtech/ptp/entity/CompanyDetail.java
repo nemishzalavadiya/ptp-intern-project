1e2995bd6a26daeb7b5e7b854afe0a3c02ed6767
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-01 13:34:52 +0530

    removed auto generated import for java.persistence

--- src/main/java/com/pirimidtech/ptp/entity/CompanyDetail.java
+++ src/main/java/com/pirimidtech/ptp/entity/CompanyDetail.java
@@ -4,9 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.*;
 import java.util.List;
 import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Table;
 
 @Getter
 @Setter

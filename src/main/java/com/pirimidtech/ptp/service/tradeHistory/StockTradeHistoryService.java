96adc6b637754d636e910b1d7a708034eef54bef
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-02 16:22:32 +0530

    removed unused import from stocktradehistoryservice

--- src/main/java/com/pirimidtech/ptp/service/tradeHistory/StockTradeHistoryService.java
+++ src/main/java/com/pirimidtech/ptp/service/tradeHistory/StockTradeHistoryService.java
@@ -2,11 +2,8 @@ package com.pirimidtech.ptp.service.tradeHistory;
 
 import com.pirimidtech.ptp.entity.StockTradeHistory;
 import com.pirimidtech.ptp.repository.StockTradeHistoryRepository;
-import jdk.nashorn.internal.runtime.options.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;

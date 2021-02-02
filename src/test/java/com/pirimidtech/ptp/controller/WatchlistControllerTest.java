de6bcdaa016a6096c5cbc4e5f481cc6a7a84723e
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-02 12:15:20 +0530

    watchlist testclass scaffolding

---
+++ src/test/java/com/pirimidtech/ptp/controller/WatchlistControllerTest.java
@@ -0,0 +1,16 @@
+package com.pirimidtech.ptp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WatchlistControllerTest {
+
+    @Test
+    void displayStockWatchlist() {
+    }
+
+    @Test
+    void displayMutualFundWatchlist() {
+    }
+}

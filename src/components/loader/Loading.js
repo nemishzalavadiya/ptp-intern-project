74d775e3e6a5ef65ff1f855de3579ff73687b060
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-19 17:24:45 +0530

    coverted css to scss

--- src/components/loader/Loading.js
+++ src/components/loader/Loading.js
@@ -1,4 +1,4 @@
-import styles from "../../styles/Loading.module.css";
+import styles from "../../styles/Loading.module.scss";
 export default function Loading() {
   return (
     <div className={styles.loading}>

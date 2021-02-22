af47f753a34c33a15ceb8359f3e891f4e954799b
darshan.gohel <darshan.gohel@pirimidtech.com>
2021-02-22 10:12:14 +0300

    Removed dashboard

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	async rewrites() {
 		return [
 			{
-				source: "/:path*",
+				source: "/api/:path*",
 				destination: "http://localhost:8080/:path*", // Proxy to Backend
 			},
 		];
--- src/service/assets.js
+++ src/service/assets.js
@@ -1,20 +1,20 @@
 import { useFetch } from "../hook/useFetch";
 
 function getAllAssets(){
-    const [isComplete,data] = useFetch('/assets?page=0&size=10');
+    const [isComplete,data] = useFetch('/api/assets?page=0&size=10');
     return [isComplete,data];
 }
 function getAssetById(id){
-    const [isComplete,data] = useFetch('/assets/'+id);
+    const [isComplete,data] = useFetch('/api/assets/'+id);
     return [isComplete,data];
 }
 function getStockByAssetId(id){
-    const [isStockFetched, stockDetail] = useFetch('/assets/stocks/'+id);
+    const [isStockFetched, stockDetail] = useFetch('/api/assets/stocks/'+id);
     console.log("stock : ", stockDetail);
     return [isStockFetched, stockDetail];
 }
 function getMfByAssetId(id){
-    const [isMfFetched, mfDetail] = useFetch('/assets/mutualfunds/'+id);
+    const [isMfFetched, mfDetail] = useFetch('/api/assets/mutualfunds/'+id);
     return [isMfFetched, mfDetail];
 }
 export {getAllAssets, getAssetById, getStockByAssetId, getMfByAssetId};

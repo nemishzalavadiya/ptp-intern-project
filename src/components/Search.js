5da75529a986f3623bed75ac42cac5aca6299fd6
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-02-25 14:16:43 +0530

    debounce method call fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,13 @@ import debounce from "src/services/debounce";
 import { Input } from "semantic-ui-react";
 
 export default function Search(props) {
+  let debounceResult = debounce(props.handleSearchChange, 300);
   return (
       <Input
         className="input-search"
         size="large"
         inverted
-        onChange={(e, data) => debounce(props.handleSearchChange(e, data), 100)}
+        onChange={(e, data) => debounceResult(e, data)}
         placeholder={props.placeholder}
         icon="search"
       />

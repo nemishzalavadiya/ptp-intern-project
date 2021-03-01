734ddc05c31419cb72bc8bd867f08f76bf0b8b9d
darshan.gohel <darshan.gohel@pirimidtech.com>
2021-03-01 16:58:54 +0300

    Chart for stock & MutualFund added

--- src/components/Mutualfund/Chart.js
+++ src/components/Mutualfund/Chart.js
@@ -1,3 +1,97 @@
+import HighStock from "highcharts/highstock";
+import React, { useState, useEffect } from "react";
+import HighchartsReact from "highcharts-react-official";
+import { Loader } from "semantic-ui-react";
+import mockData from "src/components/Mutualfund/MutualfundData";
+let mockOptions = {
+  chart: {
+    backgroundColor: "#272727",
+    borderRadius: "15px",
+    type: "spline",
+    style: {
+      color: "#fffdfd",
+    },
+    animation: {
+      duration: 1500,
+    },
+    zoomType: "x",
+  },
+  legend: {
+    enabled: false,
+  },
+  title: {
+    text: "",
+  },
+  credits: {
+    enabled: false,
+  },
+  tooltip: {
+    split: false,
+    backgroundColor: "#747474",
+    borderRadius: 20,
+    style: {
+      color: "#ffffff",
+    },
+    headerShape: "square",
+  },
+  yAxis: {
+    gridLineWidth: 0,
+    title: {
+      text: "",
+    },
+  },
+  xAxis: {
+    type: "datetime",
+    title: {
+      text: "",
+    },
+  },
+  plotOptions: {
+    line: {
+      color: "#50d688",
+    },
+  },
+  series: [
+    {
+      type: "line",
+      name: "nav",
+      data: [],
+      animation: {
+        duration: 2000,
+      },
+    },
+  ],
+};
+const transformChartData = (options, array) => {
+  const dataLength = array.length;
+  console.log(dataLength);
+  for (let i = 0; i < dataLength; i++) {
+    array[i].x = new Date(array[i].x).getTime();
+    options.series[0].data.push(array[i]);
+  }
+  return options;
+};
 export default function Chart(props) {
-  return <div className="area"></div>;
+  const [data, setData] = useState({ options: {} });
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const fetchedData = mockData;
+      const newOptions = transformChartData(mockOptions, fetchedData);
+      setData({ options: newOptions });
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="area">
+      {" "}
+      {isLoading ? (
+        <Loader active={!isLoading}></Loader>
+      ) : (
+        <HighchartsReact highcharts={HighStock} options={data.options} />
+      )}
+    </div>
+  );
 }
--- src/components/Stockdetail/Chart.js
+++ src/components/Stockdetail/Chart.js
@@ -1,3 +1,125 @@
+import HighStock from "highcharts/highstock";
+import React, { useState, useEffect } from "react";
+import HighchartsReact from "highcharts-react-official";
+import { Loader } from "semantic-ui-react";
+import mockData from "src/components/Stockdetail/StockData";
+
+let mockOptions = {
+  chart: {
+    backgroundColor: "#272727",
+    borderRadius: "15px",
+    type: "candlestick",
+    style: {
+      color: "#fffdfd",
+    },
+    animation: {
+      duration: 1500,
+    },
+    zoomType: "x",
+  },
+  credits: {
+    enabled: false,
+  },
+
+  yAxis: [
+    {
+      gridLineWidth: 0,
+      title: {
+        text: "",
+      },
+      height: "100%",
+      lineWidth: 1,
+      resize: {
+        enabled: true,
+      },
+    },
+    {
+      gridLineWidth: 0,
+      title: {
+        text: "",
+      },
+      top: "65%",
+      height: "16%",
+      offset: 0,
+      lineWidth: 1,
+    },
+  ],
+  tooltip: {
+    split: true,
+    backgroundColor: "#747474",
+    borderRadius: 20,
+    style: {
+      color: "#ffffff",
+    },
+    headerShape: "square",
+  },
+  plotOptions: {
+    candlestick: {
+      color: "#f55742",
+      upColor: "#50d688",
+    },
+    column: {
+      color: "#50d688",
+    },
+  },
+  series: [
+    {
+      type: "candlestick",
+      data: [],
+      name: "stock"
+    },
+    {
+      type: "column",
+      data: [],
+      yAxis: 1,
+      turboThresold: 0,
+    },
+  ],
+};
+const transformChartData = (options, array) => {
+  const dataLength = array.length;
+
+  for (var i = 0; i < dataLength; i += 1) {
+    options.series[0].data.push([
+      array[i][0], //date
+      array[i][1], //open
+      array[i][2], //high
+      array[i][3], //low
+      array[i][4], //close
+    ]);
+
+    options.series[1].data.push([
+      array[i][0], //date
+      array[i][5], //volume
+    ]);
+  }
+  return options;
+};
 export default function Chart(props) {
-  return <div className="area"></div>;
+  const [data, setData] = useState({ options: {} });
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const fetchedData = mockData;
+      const newOptions = transformChartData(mockOptions, fetchedData);
+      setData({ options: newOptions });
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="area">
+      {" "}
+      {isLoading ? (
+        <Loader active={!isLoading}></Loader>
+      ) : (
+        <HighchartsReact
+          highcharts={HighStock}
+          constructorType={"stockChart"}
+          options={data.options}
+        />
+      )}
+    </div>
+  );
 }

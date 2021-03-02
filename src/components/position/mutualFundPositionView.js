2cb15237c195ce65bc6bb9fd0305909337e4ac75
mahendrasinh.barad <mahendrasinh.barad@pirimidtech.com>
2021-03-02 19:15:34 +0530

    change in login message and format code

--- src/components/position/mutualFundPositionView.js
+++ src/components/position/mutualFundPositionView.js
@@ -1,48 +1,101 @@
-import React,{useState,useEffect} from 'react';
-import GridContainer from 'src/components/grid/GridContainer';
-import { getPositionByuserAndAsset} from 'src/services/position';
+import React from "react";
+import GridContainer from "src/components/grid/GridContainer";
+import { getPositionByuserAndAsset } from "src/services/position";
 import { Loader } from "semantic-ui-react";
-import {UserId} from "src/components/Objects"
-import Link from 'next/link'
+import { UserId } from "src/components/Objects";
+import Link from "next/link";
 
 const mutualFundHeaders = [
-    { header: "CompanyName", icon: "" },
-    { header: "Quantity", icon:"" },
-    { header: "Total Amount", icon: <i className="rupee sign icon small"></i> },
-    { header: "Average NAV", icon: <i className="rupee sign icon small"></i> },
-     { header: "Current NAV", icon: <i className="rupee sign icon small"></i> },
-     { header: "Current Value", icon: <i className="rupee sign icon small"></i> },
-     { header: "Profit&Loss(%)", icon: <i className="percent icon small" ></i>,showColor: true },
-     { header: "Profit&Loss", icon: <i className="rupee icon small"></i>,showColor: true },
-  ];
+  {
+    header: "CompanyName",
+    icon: "",
+  },
+  {
+    header: "Quantity",
+    icon: "",
+  },
+  {
+    header: "Total Amount",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Average NAV",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Current NAV",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Current Value",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Profit&Loss(%)",
+    icon: <i className="percent icon small"> </i>,
+    showColor: true,
+  },
+  {
+    header: "Profit&Loss",
+    icon: <i className="rupee icon small"> </i>,
+    showColor: true,
+  },
+];
 
-export default function MutualFundPosition({value,page,handlePaginationChange})
-{
-    let positionList=[];
-    let [isCompleted,response]=[false];
-    [isCompleted,response]=getPositionByuserAndAsset(UserId.userId,"mutualfund",value,page,5);
+export default function MutualFundPosition({
+  value,
+  page,
+  handlePaginationChange,
+}) {
+  let positionList = [];
+  let [isCompleted, response] = [false];
+  [isCompleted, response] = getPositionByuserAndAsset(
+    UserId.userId,
+    "mutualfund",
+    value,
+    page,
+    5
+  );
 
-    const pagination = {
-      activePage: page,
-      totalPages: response.totalPages,
-      handlePaginationChange:handlePaginationChange
-    }
+  const pagination = {
+    activePage: page,
+    totalPages: response.totalPages,
+    handlePaginationChange: handlePaginationChange,
+  };
 
-    if(isCompleted)
-    { 
-       response.content.forEach(element => {
-        console.log(element)
-           const {
-               position:{volume,price,assetDetail:{id,name}},
-               nav,
-               netValue,
-               profit,
-               
-            }=element;
-        positionList.push([<Link href={`/details/${id}`}>{name}</Link>,volume,price,price/volume,nav,netValue,profit,(profit*price)/100])
-      });
-    }
+  if (isCompleted) {
+    response.content.forEach((element) => {
+      console.log(element);
+      const {
+        position: {
+          volume,
+          price,
+          assetDetail: { id, name },
+        },
+        nav,
+        netValue,
+        profit,
+      } = element;
+      positionList.push([
+        <Link href={`/details/${id}`}> {name} </Link>,
+        volume,
+        price,
+        price / volume,
+        nav,
+        netValue,
+        profit,
+        (profit * price) / 100,
+      ]);
+    });
+  }
 
-  return (
-    !isCompleted?<Loader active/>:<GridContainer content={mutualFundHeaders} pagination={pagination} data={positionList}></GridContainer>);
+  return !isCompleted ? (
+    <Loader active />
+  ) : (
+    <GridContainer
+      content={mutualFundHeaders}
+      pagination={pagination}
+      data={positionList}
+    ></GridContainer>
+  );
 }
--- src/components/position/stockPositionView.js
+++ src/components/position/stockPositionView.js
@@ -1,43 +1,71 @@
-import React,{useState,useEffect} from 'react';
-import useWebSocket from 'src/hooks/useWebSocket';
-import GridContainer from 'src/components/grid/GridContainer';
+import React from "react";
+import useWebSocket from "src/hooks/useWebSocket";
+import GridContainer from "src/components/grid/GridContainer";
 import { Loader } from "semantic-ui-react";
 
 const stockHeaders = [
-  { header: "CompanyName", icon: "" },
-  { header: "Quantity", icon:"" },
-  { header: "Average Price", icon: <i className="rupee sign icon small"></i> },
-  { header: "Cost", icon: <i className="rupee sign icon small"></i> },
-  { header: "Current Price", icon: <i className="rupee sign icon small"></i> },
-  { header: "Current Value", icon: <i className="rupee sign icon small"></i>},
-  { header: "Profit&Loss(%)", icon: <i className="percent icon small"></i>,showColor:true },
-  { header: "Profit&Loss", icon: <i className="rupee icon small"></i>,showColor:true }
+  {
+    header: "CompanyName",
+    icon: "",
+  },
+  {
+    header: "Quantity",
+    icon: "",
+  },
+  {
+    header: "Average Price",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Cost",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Current Price",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Current Value",
+    icon: <i className="rupee sign icon small"> </i>,
+  },
+  {
+    header: "Profit&Loss(%)",
+    icon: <i className="percent icon small"> </i>,
+    showColor: true,
+  },
+  {
+    header: "Profit&Loss",
+    icon: <i className="rupee icon small"> </i>,
+    showColor: true,
+  },
 ];
-export default function StockPosition({uuid,positionList,pagination})
-{
-
+export default function StockPosition({ uuid, positionList, pagination }) {
   const [isSubscriptionCompleted, myMap] = useWebSocket(uuid);
 
-  if(isSubscriptionCompleted)
-  {
-        Array.from(myMap.values()).forEach((row,index) => {
-          if(positionList[index]!=undefined){
-            let companyData = Object.values(row);
-          positionList[index][4]=companyData[5];
-          let netValue=companyData[5]*positionList[index][1];
-          positionList[index][5]=netValue;
-          positionList[index][6]=((netValue-positionList[index][3])/(positionList[index][3])*100);
-          positionList[index][7]=netValue-positionList[index][3]
-          }
-        }); 
+  if (isSubscriptionCompleted) {
+    Array.from(myMap.values()).forEach((row, index) => {
+      if (positionList[index] != undefined) {
+        let companyData = Object.values(row);
+        positionList[index][4] = companyData[5];
+        let netValue = companyData[5] * positionList[index][1];
+        positionList[index][5] = netValue;
+        positionList[index][6] =
+          ((netValue - positionList[index][3]) / positionList[index][3]) * 100;
+        positionList[index][7] = netValue - positionList[index][3];
+      }
+    });
   }
-  return(
-  <div>
-  <div>
-    {
-       !isSubscriptionCompleted?<Loader active/>:null
-    }
-  </div>
-   <GridContainer content={isSubscriptionCompleted?stockHeaders:stockHeaders.slice(0,4)} pagination={pagination} data={positionList}></GridContainer> 
-  </div>);
-} 
+  return (
+    <div>
+      <div> {!isSubscriptionCompleted ? <Loader active /> : null} </div>
+      <GridContainer
+        content={
+          isSubscriptionCompleted ? stockHeaders : stockHeaders.slice(0, 4)
+        }
+        pagination={pagination}
+        data={positionList}
+      >
+      </GridContainer>
+    </div>
+  );
+}

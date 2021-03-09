bf0c02e2328ef80b047aa320c5fab3a07ed0eb04
darshan.gohel <darshan.gohel@pirimidtech.com>
2021-03-09 16:02:19 +0530

    modified data.sql

--- src/main/java/com/pirimidtech/ptp/entity/AssetDetail.java
+++ src/main/java/com/pirimidtech/ptp/entity/AssetDetail.java
@@ -5,13 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Getter

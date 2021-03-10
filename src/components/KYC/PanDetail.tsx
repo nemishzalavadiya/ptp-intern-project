b4e030f96712ada68e5581682defa733a2741a8e
mahendrasinh.barad <mahendrasinh.barad@pirimidtech.com>
2021-03-10 18:30:25 +0530

    kyc verification form and success screen

---
+++ src/components/KYC/PanDetail.tsx
@@ -0,0 +1,46 @@
+import React,{useState} from "react";
+import {
+  Segment,
+  Icon,
+  Header,
+  Input,
+} from "semantic-ui-react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+export default function PanDetail({panDetails,setPanDetails}) {
+
+  const onFileChange=(event)=>{
+    console.log(event);
+    if(event.target.files[0].type==="image/jpeg"||event.target.files[0].type==="image/png"){
+      setPanDetails({...panDetails,'panFile':event.target.files[0]});
+    }else
+    {
+      toast.error("Enter in PNG or JPEG Format", {
+        position: "bottom-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+      });
+    }
+  }
+
+
+  return (
+    <div>
+      <Input  placeholder="Enter PAN" value={panDetails.panNumber}  onChange={event=>setPanDetails({...panDetails,'panNumber':event.target.value})} className="kycinput" />
+      <Segment placeholder className="fileupload">
+      <Header icon>
+      <Icon name='pdf file outline' />
+      { panDetails.panFile===null?"Upload PAN":panDetails.panFile.name} 
+    </Header>
+    <label className="custom-file-upload">
+    <input type="file" onChange={onFileChange}/>
+    Upload 
+    </label>
+  </Segment>  
+      <br />
+    </div>
+  );
+}
---
+++ src/components/KYC/PersonalDetail.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Segment,
+  Step,
+  Grid,
+  Form,
+  Icon,
+  Button,
+  Divider,
+  Image,
+  Input,
+} from "semantic-ui-react";
+
+export default function PersonalDetail({personalDetails,setPersonalDetails}) {
+  return (
+    <div>
+      <Input placeholder="Enter Occupation" value={personalDetails.occupation} className="kycinput" onChange={(event)=>{setPersonalDetails({...personalDetails,occupation:event.target.value})}}/><br/>
+      <Input placeholder="Enter Youe Annual Income" value={personalDetails.annualIncome} onChange={(event)=>{setPersonalDetails({...personalDetails,annualIncome:event.target.value})}}  className="kycinput" /><br/>
+      <Input placeholder="Enter Your Mother's Name" value={personalDetails.motherName} onChange={(event)=>{setPersonalDetails({...personalDetails,motherName:event.target.value})}} className="kycinput" /><br/>
+      <Input placeholder="Enter Your Father's Name" value={personalDetails.fatherName} onChange={(event)=>{setPersonalDetails({...personalDetails,fatherName:event.target.value})}}  className="kycinput" />
+      <br />
+    </div>
+  );
+}

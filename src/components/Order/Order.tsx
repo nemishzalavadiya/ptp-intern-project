7d65a094a02bbf887121539914ae3ad9253b9154
darshan.gohel <darshan.gohel@pirimidtech.com>
2021-03-16 11:00:26 +0530

    using useEffect to reducing code

--- src/components/Order/Order.tsx
+++ src/components/Order/Order.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 import AssetOrder from "src/components/Order/AssetOrder";
 import Tab from "src/components/Tab";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { Loader, Menu, Input, Form, Grid } from "semantic-ui-react";
 import Moment from "moment";
 
 export default function Order(props) {
   const stockHeader = [
-    { header: "Company", icon: "", sortable: false },
-    { header: "Asset Class", icon: "", sortable: false },
-    { header: "Price", icon: <i className="rupee sign icon small"></i>, sortable: false },
-    { header: "Date", icon: "", sortable: false },
-    { header: "Time", icon: "", sortable: false },
-    { header: "Order Type", icon: "", sortable: false },
-    { header: "Price Type", icon: "", sortable: false },
-    { header: "Status", icon: "", sortable: false },
+    { header: "Company", icon: "" },
+    { header: "Asset Class", icon: "" },
+    { header: "Price", icon: <i className="rupee sign icon small"></i> },
+    { header: "Date", icon: "" },
+    { header: "Time", icon: "" },
+    { header: "Order Type", icon: "" },
+    { header: "Price Type", icon: "" },
+    { header: "Status", icon: "" },
   ];
   const mutualFundHeader = [
-    { header: "Company", icon: "", sortable: false },
-    { header: "Asset Class", icon: "", sortable: false },
-    { header: "Amount", icon: <i className="rupee sign icon small"></i>, sortable: false },
-    { header: "Start Date", icon: "", sortable: false },
-    { header: "Frequency", icon: "", sortable: false },
-    { header: "SIP Status", icon: "", sortable: false },
+    { header: "Company", icon: "" },
+    { header: "Asset Class", icon: "" },
+    { header: "Amount", icon: <i className="rupee sign icon small"></i> },
+    { header: "Start Date", icon: "" },
+    { header: "Frequency", icon: "" },
+    { header: "SIP Status", icon: "" },
   ];
   const [activeItem, setActiveItem] = useState(0);
   const [startDate, setStartDate] = useState(Moment().format("YYYY-MM-DD"));
@@ -34,27 +34,19 @@ export default function Order(props) {
   function handleEndDateChange(e, data) {
     setEndDate(data.value);
   }
+  const [pattern, setPattern] = useState([]);
+  const length = activeItem == 0 ? stockHeader.length : mutualFundHeader.length;;
+  useEffect(() => {
+     let initialPattern=[];
+     for(let i=0;i<length;i++){
+     initialPattern.push(0);
+     }
+     setPattern(initialPattern);
+    }, [activeItem]);
+
   function handleItemClick(index) {
     setActiveItem(index);
-    initialPattern=[];
-    if(index == 0){
-      for(let i=0;i<stockHeader.length;i++){
-        initialPattern.push(0);
-      }
-    }
-    else{
-      for(let i=0;i<mutualFundHeader.length;i++){
-        initialPattern.push(0);
-      }
-    }
-    setPattern(initialPattern);
   }
-  let initialPattern=[];
-  let length = activeItem == 0 ? stockHeader.length : mutualFundHeader.length; 
-  for(let i=0;i<length;i++){
-    initialPattern.push(0);
-  }
-  const [pattern, setPattern] = useState(initialPattern);
   const [orderBy, setOrderBy] = useState("");
   const [sortingField, setSortingField] = useState("");
   function changeArrow(index, fieldName) {
@@ -64,13 +56,13 @@ export default function Order(props) {
       midPattern.push(0);
     }
     midPattern[index] = 1 - pattern[index];
-    setPattern(midPattern);
     if (midPattern[index] == 1) {
       setOrderBy("DESC");
     } else {
       setOrderBy("ASC");
     }
     setSortingField(fieldName);
+    setPattern(midPattern);
   }
   return (
     <div>

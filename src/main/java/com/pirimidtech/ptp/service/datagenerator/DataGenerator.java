bffad85d8d492193fff0f1e4b2e80e50ee3998f4
nemish.zalavadiya <nemish.zalavadiya@pirimidtech.com>
2021-03-16 12:22:02 +0530

    mutualfund added to in generator

--- src/main/java/com/pirimidtech/ptp/service/datagenerator/DataGenerator.java
+++ src/main/java/com/pirimidtech/ptp/service/datagenerator/DataGenerator.java
@@ -1,7 +1,10 @@
 package com.pirimidtech.ptp.service.datagenerator;
 
+import com.pirimidtech.ptp.entity.AssetClass;
 import com.pirimidtech.ptp.entity.AssetDetail;
+import com.pirimidtech.ptp.entity.MutualFundStatistic;
 import com.pirimidtech.ptp.service.asset.AssetService;
+import com.pirimidtech.ptp.service.mutualfund.MutualFundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +16,7 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,43 +24,75 @@ public class DataGenerator {
     private List<Stock> stockList;
     private List<UUID> companyIdList;
     private List<String> companyNameList;
+    private List<MutualFund> mutualFundList;
 
     @Autowired
     private AssetService assetService;
+
+    @Autowired
+    private MutualFundService mutualFundService;
 
     @PostConstruct
     public void init() {
         companyIdList = new ArrayList<>();
         companyNameList = new ArrayList<>();
         stockList = new ArrayList<>();
+        mutualFundList = new ArrayList<>();
         Page<AssetDetail> assetServiceList = assetService.getAllAssetDetail(PageRequest.of(0, Integer.MAX_VALUE));
         assetServiceList.forEach(item -> {
-            companyIdList.add(item.getId());
-            companyNameList.add(item.getName());
+            if (item.getAssetClass().compareTo(AssetClass.STOCK) == 0) {
+                companyIdList.add(item.getId());
+                companyNameList.add(item.getName());
+            } else {
+                mutualFundList.add(getMutualFundFromAsset(item));
+            }
         });
     }
 
     public void setData() {
+
+        stockList.clear();
+        for (int i = 0; i < companyNameList.size(); i++) {
+            stockList.add(generateStockDataFromCompany(i));
+        }
+    }
+
+    private Stock generateStockDataFromCompany(int position) {
         DecimalFormat df = new DecimalFormat("##.##");
         df.setRoundingMode(RoundingMode.DOWN);
         float Min = 20, Max = 10000;
-        stockList.clear();
-        for (int i = 0; i < companyNameList.size(); i++) {
-            Stock stock = new Stock();
-            stock.setCompanyId(companyIdList.get(i));
-            stock.setCompanyName(companyNameList.get(i));
-            stock.setTimestamp(new Date());
-            stock.setOpen(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
-            stock.setClose(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
-            stock.setMarketPrice(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
-            stock.setHigh(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
-            stock.setLow(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
-            stock.setPercentageChange(Float.parseFloat(df.format((Math.random() * 5))));
-            stockList.add(stock);
-        }
+        Stock stock = new Stock();
+        stock.setCompanyId(companyIdList.get(position));
+        stock.setCompanyName(companyNameList.get(position));
+        stock.setTimestamp(new Date());
+        stock.setOpen(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
+        stock.setClose(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
+        stock.setMarketPrice(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
+        stock.setHigh(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
+        stock.setLow(Float.parseFloat(df.format(Min + (Math.random() * ((Max - Min) + 1)))));
+        stock.setPercentageChange(Float.parseFloat(df.format((Math.random() * 5))));
+        return stock;
     }
 
     public List<Stock> getGeneratedStockList() {
         return stockList;
+    }
+    public List<MutualFund> getGeneratedMutualFundList() {
+        return mutualFundList;
+    }
+
+    private MutualFund getMutualFundFromAsset(AssetDetail assetDetail) {
+        MutualFund mutualFund = new MutualFund();
+        Optional<MutualFundStatistic> mutualFundStatistic = mutualFundService.getMutualFundStatisticByAssetId(assetDetail.getId());
+        if (mutualFundStatistic.isPresent()) {
+            mutualFund.setCompanyId(assetDetail.getId());
+            mutualFund.setCompanyName(assetDetail.getName());
+            mutualFund.setRisk(mutualFundStatistic.get().getRisk());
+            mutualFund.setFundSize(mutualFundStatistic.get().getFundSize());
+            mutualFund.setExpenseRatio(mutualFundStatistic.get().getExpenseRatio());
+            mutualFund.setNav(mutualFundStatistic.get().getNav());
+            mutualFund.setTimestamp(new Date());
+        }
+        return mutualFund;
     }
 }
